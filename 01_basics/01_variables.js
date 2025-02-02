581f730461a17e42b5787577b8a58d9931f4e40c
Arpita <123452924+arpitasambher777@users.noreply.github.com>
2025-02-02 16:46:22 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="arpitasambher777@gmail.com"
+var accountPassword="12345"
+accountCity="una"//without datatype valid h in js
+
+accountEmail="jdjdk.com"
+accountPassword="3546"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountId,accountCity]);
+/* donot use var because
+of some issue in block scope and functional scope
+*/
+var a1;
+console.log(a1);// undefined 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as new version
+// alert(3+3) //we wre using nodejs,not browser
+console.log(3+3);
+console.log("arpita")
+let name="arpita"
+let age=20
+let f=true
+/*datatype  primitive :
+number=>2to power 53 range
+bigint:big range of int
+boolean :true/false
+null:standalone value hai :its type is object 
+undefined:variable declared nhi hai 
+symbol:used in react to check uniqueness
+
+
+nonprimitive:
+object
+*/
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof  symbol)
+
+
